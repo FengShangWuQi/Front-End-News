d863b21a08d5049cfa0842257dbb40052ddc5521
fengshangwuqi <fengshangwuqi@gmail.com>
2018-09-04 10:26:51 +0800

    docs: add 2018-09-04 :smirk_cat:

--- scripts/migrate-daily.js
+++ scripts/migrate-daily.js
@@ -14,7 +14,7 @@ const getCurrDaily = () => {
 	const date = new Date();
 	const year = date.getFullYear();
 	const month = date.getMonth() + 1;
-	const day = date.getDate();
+	const day = "" || date.getDate();
 
 	return { year, month, day };
 };
