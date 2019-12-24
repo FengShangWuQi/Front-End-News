4ad37385822ffbabffcec018b8ac39c885435bd4
fengshangwuqi <fengshangwuqi@gmail.com>
2019-12-25 00:12:56 +0800

    feat: add daily script

---
+++ scripts/daily.ts
@@ -0,0 +1,21 @@
+import * as fse from "fs-extra";
+import * as chalk from "chalk";
+
+const formatDate = (num: number) => {
+  return num < 10 ? `0${num}` : num;
+};
+
+(() => {
+  const year = new Date().getFullYear();
+  const momth = formatDate(new Date().getMonth() + 1);
+  const date = formatDate(new Date().getDate());
+
+  const data = fse.readFileSync(`${year}/${momth}/${date}.md`, "utf-8");
+  fse.outputFileSync("README.md", data, "utf8");
+
+  console.log();
+  console.log(
+    chalk.bold.inverse.green("success"),
+    chalk.yellow.bold("migrate"),
+  );
+})();
