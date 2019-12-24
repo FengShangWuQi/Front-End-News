8f70e39b38030904d391f7e251df76f7658b2dbd
fengshangwuqi <fengshangwuqi@gmail.com>
2019-12-25 00:09:33 +0800

    perf: rm migrate daily

--- scripts/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export const getCurrMonthDays = (year: number, month: number) => {
-	return new Date(year, month, 0).getDate();
-};
-
-export const formatDate = (num: number) => {
-	return num < 10 ? `0${num}` : num;
-};
