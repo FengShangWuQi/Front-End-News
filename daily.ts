6560b97e6482ca891c392897268e7ba417f4d149
fengshangwuqi <fengshangwuqi@gmail.com>
2020-06-01 17:42:55 +0800

    perf: update daily script

--- daily.ts
+++ daily.ts
@@ -1,31 +1,22 @@
-import * as fse from "fs-extra";
-import * as chalk from "chalk";
-import * as path from "path";
-
-const formatDate = (num: number) => {
-  return num < 10 ? `0${num}` : num;
-};
+import path from "path";
+import fse from "fs-extra";
+import chalk from "chalk";
+import { format } from "date-fns";
 
 (() => {
-  const now = new Date();
-
-  const year = now.getUTCFullYear();
-  const momth = formatDate(now.getUTCMonth() + 1);
-  const date = formatDate(now.getUTCDate());
-
-  const daily = `${year}/${momth}/${date}`;
+  const daily = format(new Date(), "yyyy/MM/dd");
 
   const dailyFile = path.join(__dirname, `${daily}.md`);
   const targetFile = path.join(__dirname, "public", "README.md");
   const currFile = path.join(__dirname, "public", `${daily}.md`);
 
-  fse.copy(dailyFile, targetFile, err => {
+  fse.copy(dailyFile, targetFile, (err) => {
     console.log();
 
     if (err) {
       console.log(
         chalk.bold.inverse.yellow("warning"),
-        chalk.bold.dim("today is not a work day, enjoy your short vacation"),
+        chalk.bold.dim("sorry, today has no news.")
       );
       return;
     }
@@ -34,7 +25,7 @@ const formatDate = (num: number) => {
 
     console.log(
       chalk.bold.inverse.green("success"),
-      chalk.bold.dim(`deploy - ${daily}`),
+      chalk.bold.dim(`deploy - ${daily}`)
     );
   });
 })();
