9390e96867a888e4aa6c703214fdc189e84b13a1
fengshangwuqi <fengshangwuqi@gmail.com>
2020-11-03 10:54:45 +0800

    perf: daily script

--- daily.ts
+++ daily.ts
@@ -1,31 +1,21 @@
-import path from "path";
-import fse from "fs-extra";
-import chalk from "chalk";
+import { join } from "path";
+import { copy } from "fs-extra";
 import { format } from "date-fns";
+import * as logger from "@fengshangwuqi/logger";
 
-(() => {
+(async () => {
   const daily = format(new Date(), "yyyy/MM/dd");
 
-  const dailyFile = path.join(__dirname, `${daily}.md`);
-  const targetFile = path.join(__dirname, "public", "README.md");
-  const currFile = path.join(__dirname, "public", `${daily}.md`);
+  const dailyFile = join(__dirname, `${daily}.md`);
+  const targetFile = join(__dirname, "public", "README.md");
+  const currFile = join(__dirname, "public", `${daily}.md`);
 
-  fse.copy(dailyFile, targetFile, (err) => {
-    console.log();
+  try {
+    await copy(dailyFile, targetFile);
+    await copy(dailyFile, currFile);
 
-    if (err) {
-      console.log(
-        chalk.bold.inverse.yellow("warning"),
-        chalk.bold.dim("sorry, today has no news.")
-      );
-      return;
-    }
-
-    fse.copySync(dailyFile, currFile);
-
-    console.log(
-      chalk.bold.inverse.green("success"),
-      chalk.bold.dim(`deploy - ${daily}`)
-    );
-  });
+    logger.success(`deploy - ${daily}`);
+  } catch (err) {
+    logger.warn("today has no news.");
+  }
 })();
